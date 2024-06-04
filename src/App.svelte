ef8194b9b6906348168ca8de8d8a4fc73be8783f
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-06-04 22:45:12 +0900

    chore: format

--- src/App.svelte
+++ src/App.svelte
@@ -1,56 +1,56 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-  let name = 'Svelte'
-  let imageUrl = 'https://github.com/svelte-book/sample-app/blob/main/static/react-book-1.png'
-  let altText = 'Svelte logo'
-  let disabled = true
-  let attrs = { type: 'submit', disabled: false, class: 'btn'}
+	import svelteLogo from './assets/svelte.svg';
+	import viteLogo from '/vite.svg';
+	import Counter from './lib/Counter.svelte';
+	let name = 'Svelte';
+	let imageUrl = 'https://github.com/svelte-book/sample-app/blob/main/static/react-book-1.png';
+	let altText = 'Svelte logo';
+	let disabled = true;
+	let attrs = { type: 'submit', disabled: false, class: 'btn' };
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-  <button {disabled}>送信</button>
-  <button {...attrs}>送信</button>
-  <button type={attrs.type} disabled={attrs.disabled} class={attrs.class}>送信</button>
+	<div>
+		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+			<img src={viteLogo} class="logo" alt="Vite Logo" />
+		</a>
+		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
+			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+		</a>
+	</div>
+	<h1>Vite + Svelte</h1>
+	<button {disabled}>送信</button>
+	<button {...attrs}>送信</button>
+	<button type={attrs.type} disabled={attrs.disabled} class={attrs.class}>送信</button>
 
-  <div class="card">
-    <Counter />
-  </div>
-  <div> Hello {name}</div>
-  <img src={imageUrl} alt={altText}>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+	<div class="card">
+		<Counter />
+	</div>
+	<div>Hello {name}</div>
+	<img src={imageUrl} alt={altText} />
+	<p>
+		Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer"
+			>SvelteKit</a
+		>, the official Svelte app framework powered by Vite!
+	</p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	.logo {
+		height: 6em;
+		padding: 1.5em;
+		will-change: filter;
+		transition: filter 300ms;
+	}
+	.logo:hover {
+		filter: drop-shadow(0 0 2em #646cffaa);
+	}
+	.logo.svelte:hover {
+		filter: drop-shadow(0 0 2em #ff3e00aa);
+	}
+	.read-the-docs {
+		color: #888;
+	}
 </style>

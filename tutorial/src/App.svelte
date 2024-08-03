341423294bcb9837a8da9177393f73a5f3815934
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-03 23:21:10 +0900

    feat: dynamic attribute

--- tutorial/src/App.svelte
+++ tutorial/src/App.svelte
@@ -1,5 +1,8 @@
 <script>
+	import src from './assets/svelte.svg';
 	let name = 'Svelte';
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
+<!-- src={src}と書けるが属性の名前と変数名が一致する場合、以下のように省略できる -->
+<img {src} alt="{name} name" />

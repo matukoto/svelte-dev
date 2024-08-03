788b43d7932f1a3a88b321de7e3fbb9a622f43ae
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-03 23:28:16 +0900

    feat: Nested components

--- tutorial/src/App.svelte
+++ tutorial/src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 	import src from './assets/svelte.svg';
+	import Counter from './lib/Counter.svelte';
 	let name = 'Svelte';
 </script>
 
 <h1>Hello {name.toUpperCase()}</h1>
 <!-- src={src}と書けるが属性の名前と変数名が一致する場合、以下のように省略できる -->
 <img {src} alt="{name} name" />
+<Counter />

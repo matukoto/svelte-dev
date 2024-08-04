a56804d6e0e8e39846d4b45ea381e702fe12814b
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-04 23:03:28 +0900

    Declaring props

--- tutorial/src/App.svelte
+++ tutorial/src/App.svelte
@@ -1,15 +1,5 @@
 <script>
-	let numbers = [1, 2, 3, 4];
-	function addNumber() {
-		numbers.push(numbers.length + 1);
-		// svelte5 では push するだけで値が更新されるらしい
-		numbers = numbers;
-	}
-	//
-
-	$: sum = numbers.reduce((total, currentNumber) => total + currentNumber, 0);
+	import Nested from './lib/Nested.svelte';
 </script>
 
-<p>{numbers.join(' + ')} = {sum}</p>
-
-<button on:click={addNumber}> Add a number </button>
+<Nested answer={43} />

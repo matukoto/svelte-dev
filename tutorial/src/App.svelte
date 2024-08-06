d72b07521a8cf8e3e47f92e603e522a319df2d9e
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-06 22:32:08 +0900

    else blocks

--- tutorial/src/App.svelte
+++ tutorial/src/App.svelte
@@ -9,6 +9,11 @@
 	Clicked {count}
 	{count === 1 ? 'time' : 'times'}
 </button>
+<!-- # はブロックの開始タグ -->
 {#if count > 10}
 	<p>{count} is greater than 10</p>
+	<!-- :はブロックの継続タグ -->
+{:else}
+	<p>{count} is between 0 and 10</p>
+	<!-- /はブロックの終了タグ -->
 {/if}

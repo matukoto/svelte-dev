0238b15dc4d0631a471be4467781756b92e7478c
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-08-07 21:53:13 +0900

    Component event

--- tutorial/src/App.svelte
+++ tutorial/src/App.svelte
@@ -1,1 +1,8 @@
-<button on:click|once={() => alert('clicked')}> click me</button>
+<script>
+	import Inner from './lib/Inner.svelte';
+	function handleMessage(event) {
+		alert(event.detail.text);
+	}
+</script>
+
+<Inner on:message={handleMessage} />
